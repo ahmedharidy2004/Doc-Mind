4744725c20d48b260388dd5233b3f746b53cda8a
Ahmed Haridy <ahmedunistudy@gmail.com>
2026-04-12 23:49:21 +0200

    Implement main function to load data and run pipeline

--- bridge_rag.py
+++ bridge_rag.py
@@ -1,1 +1,13 @@
+from loader import load_and_chunk
+from vector_store import create_vector_store
+from rag_pipeline import run_pipeline
 
+def main():
+    chunks = load_and_chunk("../data/mini_manual.txt")
+    create_vector_store(chunks)
+
+    query = "What departments are available in Cairo University Faculty of Engineering?"
+    run_pipeline(query, model_name="llama3:8b", k=5)
+
+if __name__ == "__main__":
+    main()
